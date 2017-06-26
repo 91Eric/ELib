e856fcd28c9338d668d07c29aecbfa3e64a6cc5d
Eric <849931616@qq.com>
2017-06-26 19:55:40 +0800

    add abstract class Pointer as top class of smartpointer

---
+++ include/Pointer.h
@@ -0,0 +1,49 @@
+/*
+* @brief : 智能指针抽象类模板实现
+* @author: Eric
+* @data:   20170626
+* @other:因为继承了object是一个抽象类，所以只要Pointer本身不实现析构函数，那它也是抽象类
+*/
+
+#ifndef _POINTER_H_
+#define _POINTER_H_
+
+#include "Object.h"
+
+namespace ELib {
+
+template<typename T>
+class Pointer : public Object
+{
+protected:
+    T* m_pointer;
+public:
+    Pointer(T* p = NULL)
+    {
+        m_pointer = NULL;
+    }
+
+    T* operator ->()
+    {
+        return m_pointer;
+    }
+
+    T& operator *()
+    {
+        return *m_pointer;
+    }
+
+    bool isNull()
+    {
+        return m_pointer == 0;
+    }
+
+    T* get()
+    {
+        return  m_pointer;
+    }
+
+};
+
+}
+#endif // POINTER_H
